878f9addeb64d0751707e9d17e3a79c8ac2a729d
Kenneth Eriksson <kenneth.eriksson@svt.se>
2020-12-04 06:54:47 +0100

    Day 4 part 1 completed

---
+++ 4/4-1.py
@@ -0,0 +1,39 @@
+passports = []
+with open('input-4.txt') as f:
+	rows = lines = f.readlines()
+	rawPassportData = ""
+	for row in rows:
+		line = row.strip()
+
+		if line == '' or row == rows[-1]:
+			
+			if row == rows[-1]:
+				rawPassportData += " %s"%line
+
+			rawPassport = rawPassportData.strip().split(' ')
+			passport = {}
+
+			for keyValuePair in rawPassport:
+				key, value = keyValuePair.split(':')
+				passport[key] = value
+
+			passports.append(passport)
+			rawPassportData = ''
+			continue
+		
+		rawPassportData += " %s"%line
+
+valids = 0
+
+requiredKeys = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'}
+for passport in passports:
+	keys = requiredKeys - set(passport.keys())
+	if ('cid' not in keys and len(keys) == 1):
+		continue
+
+	if len(keys) > 1:
+		continue
+	
+	valids += 1
+
+print valids
