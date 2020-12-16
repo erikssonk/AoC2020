775047c012131004edf0b4a9ee494d8787db6da4
Kenneth Eriksson <kenneth.eriksson@svt.se>
2020-12-16 08:46:08 +0100

    day 16, minor clean

--- 16/solution.py
+++ 16/solution.py
@@ -17,54 +17,63 @@ with open('input-16.txt') as f:
 
 rules = defaultdict(list)
 ranges = set()
+
 for line in lines[0]:
   rulename = re.match(r"(\w+( \w+)?):", line).group(1)
-  for r in re.findall(r"\d+\-\d+", line):
-    rules[rulename].append(tuple(int(x) for x in r.split("-")))
-    ranges.add(tuple(int(x) for x in r.split("-")))
+
+  for value in re.findall(r"\d+\-\d+", line):
+    rules[rulename].append(tuple(int(n) for n in value.split("-")))
+    ranges.add(tuple(int(n) for n in value.split("-")))
 
 errorRate = 0
 validTickets = []
+
 for line in lines[2]:
   ticket = [int(n) for n in line.split(",")]
+
   for num in ticket:
     valid = False
+
     for lo, hi in ranges:
       if lo <= num <= hi:
         valid = True
         break
+
     if not valid:
       errorRate += num
       break
+
   if valid:
     validTickets.append(ticket)
 
-possibleFields = {idx: set(rules.keys()) for idx in range(len(validTickets[0]))}
+possibleRules = {idx: set(rules.keys()) for idx in range(len(validTickets[0]))}
+
 for ticket in validTickets:
   for idx, value in enumerate(ticket):
-    for field in rules:
+    for rule in rules:
       possible = False
       
-      for low, high in rules[field]:
+      for low, high in rules[rule]:
         if low <= value <= high:
             possible = True
             break
+
       if not possible:
-        possibleFields[idx].discard(field)
+        possibleRules[idx].discard(rule)
 
-for idx in sorted(possibleFields, key=lambda k: len(possibleFields[k])):
-  thisField = next(iter(possibleFields[idx]))
+for idx in sorted(possibleRules, key=lambda k: len(possibleRules[k])):
+  rule = next(iter(possibleRules[idx]))
 
-  for j in possibleFields:
+  for j in possibleRules:
     if idx != j:
-      possibleFields[j].discard(thisField)
+      possibleRules[j].discard(rule)
 
-myTicket = [int(x) for x in lines[1][0].split(",")]
+myTicket = [int(n) for n in lines[1][0].split(",")]
 multiply = 1
 
-for field in possibleFields:
-  if possibleFields[field].pop().startswith("departure"):
-    multiply *= myTicket[field]
+for rule in possibleRules:
+  if possibleRules[rule].pop().startswith("departure"):
+    multiply *= myTicket[rule]
 
 print "Part 1:", errorRate 
 print "Part 2:", multiply
